2d99c0dfad37872a487e5681ca80f2e302d499ef
Felipe <fellipefreiire3@gmail.com>
2022-12-05 10:34:43 -0300

    feat: Add core components

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,19 @@
+import * as S from './styles'
+import { User } from 'phosphor-react'
+import { ComponentProps } from 'react'
+
+export interface AvatarProps extends ComponentProps<typeof S.AvatarImage> {}
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <S.AvatarContainer>
+      <S.AvatarImage {...props} />
+
+      <S.AvatarFallback delayMs={600}>
+        <User />
+      </S.AvatarFallback>
+    </S.AvatarContainer>
+  )
+}
+
+Avatar.displayName = 'Avatar'
